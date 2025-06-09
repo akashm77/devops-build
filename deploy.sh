7f2b5328060863995c36dfb969f2b09e12aa2874
akashm77 <akashm77@gmail.com>
2025-06-09 19:29:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker login -u akashm77 -p Anjali757$3@
-docker tag projectsora akashm77/projectsora1
-docker push akashm/projectsora1
+docker login -u akashm77 -p dckr_pat_adtlYmDrHcq2ShM47IYlH5luI00
+docker tag projectsora akashm77/prod
+docker push akashm77/prod
